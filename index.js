8a2ba468cb1da779625f4bde5f33802d6c3448c8
kevinderitis <kevin.deritis@hotmail.com>
2022-06-21 20:14:02 -0300

    Este es el cambio de 2

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express();
 
 
-app.get('/', (req, res) => res.send('Esta es la ruta raiz') )
+app.get('/', (req, res) => res.send('Esta es la ruta raiz que modifique ') )
 
 app.listen(8080, () => console.log('server running'))
